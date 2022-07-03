f7c55713111d863392689d7e6be08e02e7685758
srezal <sereshka0967@gmail.com>
2022-07-04 00:38:58 +0300

    move plt and fig to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from models.RabbitsPopulationModel import RabbitsPopulationModel
+from plots.RabbitsPopulationPlot import RabbitsPopulationPlot
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    rb = RabbitsPopulationModel()
-    rb.run()
+    plt.style.use("dark_background")
+    fig = plt.figure(figsize=(8, 6))
+    rb = RabbitsPopulationPlot(fig)
+    plt.show()
